6d7686b888c0ac963191281b1ad1348e0a940868
Your Name <you@example.com>
2024-11-09 13:33:57 +0530

    in this branch mongoose  connect

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 
 const express = require('express');
 const app = express();
-app.get("/", function (req, res) {
-    res.send("This is homepage ");
-});
-app.get("/data", function (req, res) {
-    res.send("This is data page");
-});
-app.get("/data/notes", function (req, res) {
-    res.send("This is notes for you");
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://deep:deep123@cluster0.bwoiy.mongodb.net/notesdb").then(function () {
+
+    app.get("/", function (req, res) {
+        res.send("This is homepage ");
+    });
+    app.get("/data", function (req, res) {
+        res.send("This is data page");
+    });
+    app.get("/data/notes", function (req, res) {
+        res.send("This is notes for you");
+    });
+    console.log("I am started mongooes connect");
 });
 
 app.listen(5001, function () {
